b91fce446a407220e62e659a95eafd18a3051958
pank3004 <pankajkumarewpk2004@gmail.com>
2023-12-05 17:46:56 +0530

    j

---
+++ oops/8.Firend Function/4_practice_question_simple_.cpp
@@ -0,0 +1,36 @@
+// add two number (that belong the differet-defferent classes)
+
+#include<iostream>
+using namespace std;
+
+class Y;    // forward declaration of class Y
+class X {
+    int num1;
+public: 
+    void setData(int value){
+        num1=value;
+    }
+    friend void add(X ,Y);
+
+};
+
+class Y {
+    int num2;
+public: 
+    void setData(int value){
+        num2=value;
+    }
+    friend void add(X ,Y);
+};
+
+void add(X o1, Y o2){
+    cout<<"add of the two number: "<<(o1.num1+o2.num2)<<endl;
+}
+int main() {
+    X o1;
+    Y o2;
+    o1.setData(4);
+    o2.setData(89);
+    add(o1,o2);
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,48 +1,112 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Complex;
-class Calculator{
-public: 
-    int sumRealComplex(Complex o1, Complex o2);
-    int sumCompComplex(Complex o1, Complex o2);
+class Y;
+class X
+{
+    int data1;
+    friend void exchange(X &, Y &);
 
-};
-class Complex {
-    int a;
-    int b;
-    friend int Calculator :: sumRealComplex(Complex o1, Complex o2);
-    friend int Calculator :: sumCompComplex(Complex o1, Complex o2);
-
-public: 
-    void setData(int x, int y){
-        a=x;
-        b=y;
+public:
+    void setData(int value)
+    {
+        data1 = value;
     }
-    void getData(){
-        cout<<"this is the complex no : "<<a<<" + "<<b<<"i"<<endl;
+    void getData()
+    {
+        cout << "the value of data1 is: " << data1 << endl;
     }
 };
 
-int Calculator:: sumRealComplex(Complex o1, Complex o2){
-    return (o1.a + o2.a);
-}
-int Calculator:: sumCompComplex(Complex o1, Complex o2){
-    return (o1.b + o2.b);
+class Y
+{
+    int data2;
+    friend void exchange(X &, Y &);
+
+public:
+    void setData(int value)
+    {
+        data2 = value;
+    }
+    void getData()
+    {
+        cout << "the value of data2 is: " << data2 << endl;
+    }
+};
+
+void exchange(X &oX, Y &oY)
+{
+    int temp = oX.data1;
+    oX.data1 = oY.data2;
+    oY.data2 = temp;
 }
 
-int main() {
-    Complex c1, c2;
-    c1.setData(3,5);
-    c1.getData();
+int main()
+{
+    X oX;
+    Y oY;
+    oX.setData(34);
+    oY.setData(76);
 
-    c2.setData(4,5);
-    c2.getData();
-    Calculator cal;
-    int sumReal=cal.sumRealComplex(c1,c2);
-    cout<<"this sum of the real parts of the complex numbers: "<<sumReal<<endl;
-    int sumComp=cal.sumCompComplex(c1,c2);
-    cout<<"this sum of the complex parts of the complex numbers: "<<sumComp<<endl;
+    cout << "Before exchange the values of data1 and data2:";
+    oX.getData();
+    oY.getData();
+
+    exchange(oX, oY);
+    
+    cout << "After exchange the values of data1 and data2:";
+    oX.getData();
+    oY.getData();
 
     return 0;
 }
+
+
+// #include<iostream>
+// using namespace std;
+
+// class Complex;
+// class Calculator{
+// public: 
+//     int sumRealComplex(Complex o1, Complex o2);
+//     int sumCompComplex(Complex o1, Complex o2);
+
+// };
+// class Complex {
+//     int a;
+//     int b;
+//     friend int Calculator :: sumRealComplex(Complex o1, Complex o2);
+//     friend int Calculator :: sumCompComplex(Complex o1, Complex o2);
+
+// public: 
+//     void setData(int x, int y){
+//         a=x;
+//         b=y;
+//     }
+//     void getData(){
+//         cout<<"this is the complex no : "<<a<<" + "<<b<<"i"<<endl;
+//     }
+// };
+
+// int Calculator:: sumRealComplex(Complex o1, Complex o2){
+//     return (o1.a + o2.a);
+// }
+// int Calculator:: sumCompComplex(Complex o1, Complex o2){
+//     return (o1.b + o2.b);
+// }
+
+// int main() {
+//     Complex c1, c2;
+//     c1.setData(3,5);
+//     c1.getData();
+
+//     c2.setData(4,5);
+//     c2.getData();
+//     Calculator cal;
+//     int sumReal=cal.sumRealComplex(c1,c2);
+//     cout<<"this sum of the real parts of the complex numbers: "<<sumReal<<endl;
+//     int sumComp=cal.sumCompComplex(c1,c2);
+//     cout<<"this sum of the complex parts of the complex numbers: "<<sumComp<<endl;
+
+//     return 0;
+// }
